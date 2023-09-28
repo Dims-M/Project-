f7926bd6fb92a04046817bff3411ea4f9ffe42e6
Dim <dimsxm@yandex.ru>
2023-09-28 17:17:57 +0300

    добавил библиотеку  для запросов  cpr

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,20 @@
 //Директива подпроцессора
 #include<iostream>
+#include <cpr/cpr.h>
 
 //Подключаем библиотеки. Пространство имен. Нужный функционал.
 using namespace std;
- 
+
 
 void main()
 {
 	setlocale(LC_ALL,"Rus");
 	/*system("chcp 1251>nul");*/
 	cout << "Запуск программы" <<endl ; 
+
+	//Get запрос к API
+	
+	auto r = cpr::Get(cpr::Url{ "http://185.87.48.87/api/мusictrackinfo/getallproducts/getallproducts" });
 
 
 }
